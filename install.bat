40b55eec63c7c17e96812476d7af68d0940e9b6e
Marius Juston <Marius.juston@hotmail.fr>
2024-02-07 14:56:59 -0600

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,3 +2,9 @@ winget install "JetBrains Toolbox"
 winget  install 9WZDNCRDK3WP #Slack
 winget install Anaconda3
 winget install "Microsoft Visual Studio Code"
+winget install Git.Git
+
+wsl --install -d Ubuntu-18.04
+wsl --install -d Ubuntu-22.04
+
+pause
