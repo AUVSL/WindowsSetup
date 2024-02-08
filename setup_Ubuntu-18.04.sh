3e63160634186b3bb9d847ff5e1b9b3a16f41507
Marius-Juston <marius.juston@hotmail.fr>
2024-02-08 01:29:44 -0600

    updated ubuntu setup file to include automatic saying -y

--- setup_Ubuntu-18.04.sh
+++ setup_Ubuntu-18.04.sh
@@ -7,15 +7,21 @@ sudo apt install curl # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 sudo apt update
-sudo apt install ros-melodic-desktop-full
+sudo apt install ros-melodic-desktop-full -y
 
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 
-sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
-sudo apt install python-rosdep
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential  -y
+sudo apt install python-rosdep  -y
 
-sudo apt-get install ros-melodic-jackal-simulator ros-melodic-jackal-desktop ros-melodic-jackal-navigation
-sudo apt-get install ros-melodic-husky-simulator
+sudo apt-get install ros-melodic-jackal-simulator ros-melodic-jackal-desktop ros-melodic-jackal-navigation  -y
+sudo apt-get install ros-melodic-husky-simulator  -y
 
 sudo rosdep init
 rosdep update
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+mkdir ~/catkin_ws/src -p
+code ~/catkin_ws/src
--- setup_Ubuntu-22.04.sh
+++ setup_Ubuntu-22.04.sh
@@ -2,15 +2,15 @@
 
 locale  # check for UTF-8
 
-sudo apt update && sudo apt install locales
+sudo apt update && sudo apt install locales -y
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 locale  # verify settings
 
-sudo apt install software-properties-common
-sudo add-apt-repository universe
+sudo apt install software-properties-common  -y
+sudo add-apt-repository universe -y
 
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
@@ -22,20 +22,18 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-a
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install ros-humble-desktop
+sudo apt install ros-humble-desktop  -y
 
-sudo apt install ros-humble-clearpath-desktop
-sudo apt-get install ros-humble-clearpath-simulator
+sudo apt install ros-humble-clearpath-desktop  -y
+sudo apt-get install ros-humble-clearpath-simulator  -y
 
 # Replace ".bash" with your shell if you're not using bash
 # Possible values are: setup.bash, setup.sh, setup.zsh
 
 echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 
-
-mkdir ~/catkin_ws/src -p
-
-code ~/catkin_ws/src
-
 sudo apt-get update
 sudo apt-get upgrade -y
+
+mkdir ~/catkin_ws/src -p
+code ~/catkin_ws/srcauvsl
