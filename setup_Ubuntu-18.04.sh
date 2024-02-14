da52668c61f823805751ea35b7962b455f14ec4c
Marius Juston <Marius.juston@hotmail.fr>
2024-02-14 17:37:31 -0600

    Update setup_Ubuntu-18.04.sh

--- setup_Ubuntu-18.04.sh
+++ setup_Ubuntu-18.04.sh
@@ -3,7 +3,7 @@
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d$
 
-sudo apt install curl # if you haven't already installed curl
+sudo apt install curl -y # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 sudo apt update
